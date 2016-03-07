afe7a3f0ad351b8743f36082fbfd6ff089be6494
Kenshin <fxblog@gmail.com>
2016-03-07 15:15:59 +0800

    Modify test curl.New() code.

--- curl_test.go
+++ curl_test.go
@@ -36,17 +36,27 @@ func TestCurl(t *testing.T) {
 	*/
 
 	// multi download
-	dl := make(Download)
-	dl[0] = Detail{"http://npm.taobao.org/mirrors/node/latest/node.exe", "node.exe", os.TempDir() + "/" + "node.exe"}
-	dl[1] = Detail{"http://npm.taobao.org/mirrors/node/v5.7.1/win-x64/node.exe", "node2.exe", os.TempDir() + "/" + "node2.exe"}
-	dl[2] = Detail{"http://npm.taobao.org/mirrors/node/v5.7.0/win-x64/node.exe", "node3.exe", os.TempDir() + "/" + "node3.exe"}
-	dl[3] = Detail{"http://golangtc.com/static/go/1.6/go1.6.windows-amd64.zip", "windows-amd64.zip", os.TempDir() + "/" + "windows-amd64.zip"}
-	dl[4] = Detail{"http://npm.taobao.org/mirrors/node/v4.0.0/win-x86/node.exe", "node5.exe", os.TempDir() + "/" + "node5.exe"}
-	dl[5] = Detail{"http://npm.taobao.org/mirrors/node/v4.1.0/win-x86/node.exe", "node6.exe", os.TempDir() + "/" + "node6.exe"}
-	dl[6] = Detail{"http://npm.taobao.org/mirrors/node/v4.2.0/win-x86/node.exe", "node7.exe", os.TempDir() + "/" + "node7.exe"}
-	dl[7] = Detail{"http://npm.taobao.org/mirrors/node/v4.3.0/win-x86/node.exe", "node8.exe", os.TempDir() + "/" + "node8.exe"}
-	dl[8] = Detail{"http://npm.taobao.org/mirrors/node/v4.3.0/win-x86/node.exe", "node9.exe", os.TempDir() + "/" + "node9.exe"}
-	dl[9] = Detail{"http://npm.taobao.org/mirrors/node/v4.3.0/win-x86/node.exe", "node10.exe", os.TempDir() + "/" + "node10.exe"}
+	dl := Download{
+		Detail{"http://npm.taobao.org/mirrors/node/latest/node.exe", "node.exe", os.TempDir() + "/" + "node.exe"},
+		Detail{"http://npm.taobao.org/mirrors/node/v5.7.1/win-x64/node.exe", "node2.exe", os.TempDir() + "/" + "node2.exe"},
+	}
+	dl = dl.Add(Detail{"http://npm.taobao.org/mirrors/node/v5.7.0/win-x64/node.exe", "node3.exe", os.TempDir() + "/" + "node3.exe"})
+	dl = dl.Add(Detail{"https://www.google.com/intl/zh-CN/chrome/browser/?standalone=1&extra=devchannel&platform=win64", "ChromeSetup.zip", os.TempDir() + "/" + "ChromeSetup.zip"})
+	dl = dl.Add(Detail{"http://golangtc.com/static/go/1.6/go1.6.windows-amd64.zip", "windows-amd64.zip", os.TempDir() + "/" + "windows-amd64.zip"})
+
+	/*
+		dl[0] = Detail{"http://npm.taobao.org/mirrors/node/latest/node.exe", "node.exe", os.TempDir() + "/" + "node.exe"}
+		dl[1] = Detail{"http://npm.taobao.org/mirrors/node/v5.7.1/win-x64/node.exe", "node2.exe", os.TempDir() + "/" + "node2.exe"}
+		dl[2] = Detail{"http://npm.taobao.org/mirrors/node/v5.7.0/win-x64/node.exe", "node3.exe", os.TempDir() + "/" + "node3.exe"}
+		dl[3] = Detail{"http://npm.taobao.org/mirrors/node/v4.0.0/win-x86/node.exe", "node5.exe", os.TempDir() + "/" + "node5.exe"}
+		dl[4] = Detail{"http://npm.taobao.org/mirrors/node/v4.1.0/win-x86/node.exe", "node6.exe", os.TempDir() + "/" + "node6.exe"}
+		dl[5] = Detail{"http://npm.taobao.org/mirrors/node/v4.2.0/win-x64/node.exe", "node7.exe", os.TempDir() + "/" + "node7.exe"}
+		dl[6] = Detail{"http://npm.taobao.org/mirrors/node/v4.3.0/win-x86/node.exe", "node8.exe", os.TempDir() + "/" + "node8.exe"}
+		dl[7] = Detail{"http://npm.taobao.org/mirrors/node/v4.3.1/win-x64/node.exe", "node9.exe", os.TempDir() + "/" + "node9.exe"}
+		dl[8] = Detail{"http://npm.taobao.org/mirrors/node/v4.3.2/win-x86/node.exe", "node10.exe", os.TempDir() + "/" + "node10.exe"}
+		dl[9] = Detail{"http://npm.taobao.org/mirrors/node/v4.2.2/win-x64/node.exe", "node11.exe", os.TempDir() + "/" + "node11.exe"}
+		dl[10] = Detail{"http://npm.taobao.org/mirrors/node/v4.2.1/win-x86/node.exe", "node12.exe", os.TempDir() + "/" + "node12.exe"}
+	*/
 	code := New(dl)
 	fmt.Printf("curl.New return code is %v\n", code)
 
