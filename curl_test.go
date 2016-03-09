bfee82dba1e80da8f9d12834f200d131f2c96349
Kenshin <fxblog@gmail.com>
2016-03-09 16:36:48 +0800

    Optimize test code.

--- curl_test.go
+++ curl_test.go
@@ -28,16 +28,16 @@ func TestCurl(t *testing.T) {
 	}
 
 	// simple download
-	newDL, err := New("http://npm.taobao.org/mirrors/node/latest/node.exe", "node.exe", os.TempDir()+"/"+"node.exe")
+	newDL, err := New("http://npm.taobao.org/mirrors/node/latest/node.exe", "node.exe", os.TempDir())
 	fmt.Printf("curl.New return ld  is %v\n", newDL)
 	fmt.Printf("curl.New return err is %v\n", err)
 
 	// multi download
 	dl := Download{}
 	ts := new(Task)
-	dl.AddTask(ts.New("http://npm.taobao.org/mirrors/node/latest/node.exe", "node.exe", os.TempDir()+"/"+"node.exe"))
-	dl.AddTask(ts.New("http://npm.taobao.org/mirrors/node/v5.7.0/win-x64/node.exe", "node3.exe", os.TempDir()+"/"+"node3.exe"))
-	dl.AddTask(ts.New("https://www.google.com/intl/zh-CN/chrome/browser/?standalone=1&extra=devchannel&platform=win64", "ChromeSetup.zip", os.TempDir()+"/"+"ChromeSetup.zip"))
+	dl.AddTask(ts.New("http://npm.taobao.org/mirrors/node/latest/node.exe", "node.exe", os.TempDir()))
+	dl.AddTask(ts.New("http://npm.taobao.org/mirrors/node/v5.7.0/win-x64/node.exe", "node4.exe", os.TempDir()))
+	dl.AddTask(ts.New("https://www.google.com/intl/zh-CN/chrome/browser/?standalone=1&extra=devchannel&platform=win64", "ChromeSetup.zip", os.TempDir()))
 
 	newDL, err = New(dl)
 	fmt.Printf("curl.New return ld  is %v\n", newDL)
