1479ba53aac6a1cabc3505c45ebe8ea886d8630d
Kenshin <fxblog@gmail.com>
2016-03-09 11:00:34 +0800

    Modify test code.

--- curl_test.go
+++ curl_test.go
@@ -2,7 +2,6 @@ package curl
 
 import (
 	"fmt"
-	//"io"
 	"os"
 	"testing"
 )
@@ -36,13 +35,12 @@ func TestCurl(t *testing.T) {
 	*/
 
 	// multi download
-	dl := Download{
-		Detail{"http://npm.taobao.org/mirrors/node/latest/node.exe", "node.exe", os.TempDir() + "/" + "node.exe"},
-		Detail{"http://npm.taobao.org/mirrors/node/v5.7.1/win-x64/node.exe", "node2.exe", os.TempDir() + "/" + "node2.exe"},
-	}
-	dl = dl.Add(Detail{"http://npm.taobao.org/mirrors/node/v5.7.0/win-x64/node.exe", "node3.exe", os.TempDir() + "/" + "node3.exe"})
-	dl = dl.Add(Detail{"https://www.google.com/intl/zh-CN/chrome/browser/?standalone=1&extra=devchannel&platform=win64", "ChromeSetup.zip", os.TempDir() + "/" + "ChromeSetup.zip"})
-	dl = dl.Add(Detail{"http://golangtc.com/static/go/1.6/go1.6.windows-amd64.zip", "windows-amd64.zip", os.TempDir() + "/" + "windows-amd64.zip"})
+	dl := Download{}
+	dl.AddTask(Detail{"http://npm.taobao.org/mirrors/node/latest/node.exe", "node.exe", os.TempDir() + "/" + "node.exe", 0})
+	//dl.AddTask(Detail{"http://npm.taobao.org/mirrors/node/v5.7.0/win-x64/node.exe", "node3.exe", os.TempDir() + "/" + "node3.exe", 0})
+	//dl.AddTask(Detail{"http://npm.taobao.org/mirrors/node/v5.7.1/win-x64/node.exe", "node2.exe", os.TempDir() + "/" + "node2.exe", 0})
+	//dl.AddTask(Detail{"https://www.google.com/intl/zh-CN/chrome/browser/?standalone=1&extra=devchannel&platform=win64", "ChromeSetup.zip", os.TempDir() + "/" + "ChromeSetup.zip", 0})
+	//dl.AddTask(Detail{"http://golangtc.com/static/go/1.6/go1.6.windows-amd64.zip", "windows-amd64.zip", os.TempDir() + "/" + "windows-amd64.zip", 0})
 
 	/*
 		dl[0] = Detail{"http://npm.taobao.org/mirrors/node/latest/node.exe", "node.exe", os.TempDir() + "/" + "node.exe"}
@@ -57,7 +55,8 @@ func TestCurl(t *testing.T) {
 		dl[9] = Detail{"http://npm.taobao.org/mirrors/node/v4.2.2/win-x64/node.exe", "node11.exe", os.TempDir() + "/" + "node11.exe"}
 		dl[10] = Detail{"http://npm.taobao.org/mirrors/node/v4.2.1/win-x86/node.exe", "node12.exe", os.TempDir() + "/" + "node12.exe"}
 	*/
-	code := New(dl)
-	fmt.Printf("curl.New return code is %v\n", code)
+	newDL, err := New(dl)
+	fmt.Printf("curl.New return ld  is %v\n", newDL)
+	fmt.Printf("curl.New return err is %v\n", err)
 
 }
