aac4acb13ae4c30533a9ede9d8f177184bda1ebb
Kenshin <fxblog@gmail.com>
2016-03-10 15:03:06 +0800

    Add test code.

--- curl_test.go
+++ curl_test.go
@@ -28,19 +28,38 @@ func TestCurl(t *testing.T) {
 	}
 
 	// simple download
-	newDL, err := New("http://npm.taobao.org/mirrors/node/latest/node.exe", "node.exe", os.TempDir())
+	newDL, err := New("http://npm.taobao.org/mirrors/node/v0.10.26/node.exe")
 	fmt.Printf("curl.New return ld  is %v\n", newDL)
 	fmt.Printf("curl.New return err is %v\n", err)
 
 	// multi download
 	dl := Download{}
 	ts := new(Task)
-	dl.AddTask(ts.New("http://npm.taobao.org/mirrors/node/latest/node.exe", "node.exe", os.TempDir()))
+	dl.AddTask(ts.New("http://npm.taobao.org/mirrors/node/latest/node.exe", "nodeeeeeeeeeeeeeeeeeeeeeeee.exe", os.TempDir()))
 	dl.AddTask(ts.New("http://npm.taobao.org/mirrors/node/v5.7.0/win-x64/node.exe", "node4.exe", os.TempDir()))
 	dl.AddTask(ts.New("https://www.google.com/intl/zh-CN/chrome/browser/?standalone=1&extra=devchannel&platform=win64", "ChromeSetup.zip", os.TempDir()))
-
 	newDL, err = New(dl)
+
 	fmt.Printf("curl.New return ld  is %v\n", newDL)
 	fmt.Printf("curl.New return err is %v\n", err)
+
+	/*
+		dl := Download{}
+
+		url := "http://npm.taobao.org/mirrors/node/latest/node.exe"
+		parseArgs(&dl, errStack, url)
+
+		ts := Task{}
+		ts1 := ts.New("http://npm.taobao.org/mirrors/node/v5.7.0/win-x64/node.exe", "node4.exe", os.TempDir())
+		ts2 := ts.New("http://npm.taobao.org/mirrors/node/v5.7.2/win-x64/node.exe", "node2.exe", os.TempDir())
+		ts3 := ts.New("http://npm.taobao.org/mirrors/node/v5.7.3/win-x64/node.exe", "node3.exe", os.TempDir())
+		parseArgs(&dl, errStack, ts1, ts2, ts3)
+
+		ts := new(Task)
+		dl.AddTask(ts.New("http://npm.taobao.org/mirrors/node/latest/node.exe", "node.exe", os.TempDir()))
+		dl.AddTask(ts.New("http://npm.taobao.org/mirrors/node/v5.7.0/win-x64/node.exe", "node4.exe", os.TempDir()))
+		dl.AddTask(ts.New("https://www.google.com/intl/zh-CN/chrome/browser/?standalone=1&extra=devchannel&platform=win64", "ChromeSetup.zip", os.TempDir()))
+		parseArgs(&dl, dl)
+	*/
 
 }
