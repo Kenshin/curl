37428e1f019514a11e3d8c6cb102593f75dfea47
Kenshin <fxblog@gmail.com>
2016-03-10 14:39:54 +0800

    Add safe print line.

--- curl.go
+++ curl.go
@@ -273,8 +273,9 @@ func download(ts *Task, line, max int, errStack *[]CurlError) {
 				ts.Code = -5
 			}
 			curStack(line, max)
-			empty := strings.Repeat(" ", 100)
-			fmt.Printf("\r%v download error.%v", name, empty)
+			msg := fmt.Sprintf("%v download error.", safeName(name))
+			empty := strings.Repeat(" ", 80-len(msg))
+			fmt.Printf("\r%v%v", msg, empty)
 		}
 	}()
 
@@ -363,7 +364,9 @@ func safeName(name string) string {
 func progressbar(name string, start time.Time, i int, suffix string) {
 	h := strings.Repeat("=", i) + ">" + strings.Repeat("_", 50-i)
 	d := time.Now().Sub(start)
-	fmt.Printf("\r%v %.0f%% [%s] %v"+suffix, safeName(name), float32(i)/50*100, h, time.Duration(d.Seconds())*time.Second)
+	s := fmt.Sprintf("%v %.0f%% [%s] %v", safeName(name), float32(i)/50*100, h, time.Duration(d.Seconds())*time.Second)
+	e := strings.Repeat(" ", 80-len(s))
+	fmt.Printf("\r%v%v%v", s, suffix, e)
 }
 
 func curStack(line, max int) {
