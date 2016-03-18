ddfd4c695cf44bfbc99c543d10319f8e2a0de1a4
Kenshin <fxblog@gmail.com>
2016-03-18 10:52:35 +0800

    Move same function to curl_util.go.

--- curl.go
+++ curl.go
@@ -115,7 +115,6 @@ func Get(url string) (code int, res *http.Response, err error) {
 	}
 
 	return 0, res, err
-
 }
 
 // Read line from io.ReadCloser
@@ -251,26 +250,6 @@ func New(args ...interface{}) (dl Download, errStack []CurlError) {
 	return
 }
 
-func parseArgs(args ...interface{}) (int, Download) {
-	dl := Download{}
-	if len(args) == 0 {
-		panic(CurlError{"curl.New()", -6, "curl.New() parameter type error."})
-	} else {
-		switch args[0].(type) {
-		case string:
-			url, title, name, dst := safeArgs(args...)
-			dl.AddTask(Task{url, title, name, dst, 0})
-		case Task:
-			for _, v := range args {
-				dl.AddTask(v.(Task))
-			}
-		case Download:
-			dl = args[0].(Download)
-		}
-	}
-	return len(dl), dl
-}
-
 func download(ts *Task, line, max int, errStack *[]CurlError) {
 	url, title, name, dst := ts.Url, ts.Title, ts.Name, safeDst(ts.Dst)
 	defer func() {
@@ -353,83 +332,6 @@ func download(ts *Task, line, max int, errStack *[]CurlError) {
 			progressbar(title, start, 50, "")
 		}
 	}
-}
-
-func isDirExist(path string) bool {
-	_, err := os.Stat(path)
-	if err != nil {
-		return os.IsExist(err)
-	} else {
-		return true
-	}
-}
-
-func isBodyBytes(content string) (isBytes bool) {
-	if strings.Index(content, "json") != -1 {
-		isBytes = false
-	} else if strings.Index(content, "text") != -1 {
-		isBytes = false
-	} else if strings.Index(content, "application") != -1 {
-		isBytes = true
-	}
-	return
-}
-
-func maxTitleLength(titles []string) int {
-	max := 0
-	for _, v := range titles {
-		if len(v) > max {
-			max = len(v)
-		}
-	}
-	if max > 15 {
-		max = 15
-	}
-	return max
-}
-
-func safeTitle(title string) string {
-	h := ""
-	if len(title) > 15 {
-		title = title[:12] + "..."
-	} else if len(title) <= maxNameLen {
-		h = strings.Repeat(" ", maxNameLen-len(title))
-	}
-	return h + title + ":"
-}
-
-func safeDst(dst string) string {
-	if !strings.HasSuffix(dst, "/") {
-		dst += "/"
-	}
-	return dst
-}
-
-/*
- arg1: url
- arg2: title
- arg3: name
- arg4: dst
-*/
-func safeArgs(args ...interface{}) (url, title, name, dst string) {
-	url = args[0].(string)
-	switch len(args) {
-	case 1:
-		names := strings.Split(url, "/")
-		title = names[len(names)-1:][0]
-		name = title
-		dst, _ = os.Getwd()
-	case 2:
-		title = args[1].(string)
-		name = title
-		dst, _ = os.Getwd()
-	case 3:
-		title, name = args[1].(string), args[2].(string)
-		dst, _ = os.Getwd()
-	case 4:
-		title, name, dst = args[1].(string), args[2].(string), args[3].(string)
-	}
-	return
 }
 
 /*
